c49c6def7ac19009b179826aa94660ecfcd0fb44
Alex <faqmybrain@gmail.com>
2015-08-01 10:54:57 +0300

    Stylized Products page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,11 @@
 Product.delete_all
 
 Product.create!(title: 'Programming Ruby 1.9 & 2.0',
-                description:
-                    %{<p>
-                        Ruby is the fastest growing and most exciting dynamic language out there. If you need to get working programs delivered fast, you should add Ruby to your toolbox.
-                    </p>},
+                description: %{<p>Ruby is the fastest growing and most exciting dynamic language out there. If you need to get working programs delivered fast, you should add Ruby to your toolbox.</p>},
                 image_url: 'ruby.jpg',
                 price: 49.95)
+
+Product.create!(title: 'What is CoffeeScript?',
+                description: %{<p>CoffeeScript is a little language that compiles down to JavaScript. The syntax is inspired by Ruby and Python, and implements many features from those two languages.</p>},
+                image_url: 'cofee.gif',
+                price: 32.95)
